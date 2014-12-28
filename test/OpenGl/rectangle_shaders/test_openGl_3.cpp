b68abb6c21a196308633383e50eec998ff075a7f
Vladimir Stalmakov <akummail@gmail.com>
2014-12-28 21:43:56 +0300

    Правки по OpenGl3

--- test/OpenGl/rectangle_shaders/test_openGl_3.cpp
+++ test/OpenGl/rectangle_shaders/test_openGl_3.cpp
@@ -1,5 +1,6 @@
 #ifdef _WIN32
 #include <windows.h>
+
 #endif
 #include <memory>
 #include <iostream>
@@ -7,143 +8,204 @@
 #include <vector>
 #include <SDL.h>
 #include "GL/glew.h"
+
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 using std::cout;
 using std::endl;
 
+#define OpenGLCheckErrorMacro()( OpenGLCheckError(__FILE__, __LINE__))
+
+void OpenGLCheckError(std::string file, int rowNuber) {
+    GLenum errorCode = glGetError();
+    if (errorCode != GL_NO_ERROR) {
+        std::string resultError;
+        switch (errorCode) {
+            case
+                GL_INVALID_ENUM : {
+                resultError = "GL_INVALID_ENUM : An unacceptable value is specified for an enumerated argument. The offending command is ignored and has no other side effect than to set the error flag.";
+            }
+            case
+                GL_INVALID_VALUE: {
+                resultError = "GL_INVALID_VALUE : A numeric argument is out of range. The offending command is ignored and has no other side effect than to set the error flag.";
+            }
+            case
+                GL_INVALID_OPERATION: {
+                resultError = "GL_INVALID_OPERATION : The specified operation is not allowed in the current state. The offending command is ignored and has no other side effect than to set the error flag.";
+            }
+            case
+                GL_INVALID_FRAMEBUFFER_OPERATION: {
+                resultError = "GL_INVALID_FRAMEBUFFER_OPERATION : The framebuffer object is not complete. The offending command is ignored and has no other side effect than to set the error flag.";
+            }
+            case
+                GL_OUT_OF_MEMORY: {
+                resultError = "GL_OUT_OF_MEMORY : There is not enough memory left to execute the command. The state of the GL is undefined, except for the state of the error flags, after this error is recorded.";
+            }
+            case
+                GL_STACK_UNDERFLOW: {
+                resultError = "GL_STACK_UNDERFLOW : An attempt has been made to perform an operation that would cause an internal stack to underflow.";
+            }
+            case
+                GL_STACK_OVERFLOW: {
+                resultError = "GL_STACK_OVERFLOW : An attempt has been made to perform an operation that would cause an internal stack to overflow.";
+            }
+            default:
+                resultError = "OpenGL ERROR ";
+        }
+        cout << "Ошибка OpenGL: " << errorCode << " " << resultError << " Файл: " << file << " Строка " << rowNuber << endl;
+    }
+}
+enum ShaderType {
+    Vertex,
+    Fragment
+};
+
+
+class Shader {
+
+public:
+
+    Shader(GLuint _shaderId) : _shaderId(_shaderId) {
+    }
+
+    GLuint get_shaderId() const {
+        return _shaderId;
+    }
+
+    virtual ~Shader() {
+        glDeleteShader(_shaderId);
+    }
+private:
+    GLuint _shaderId;
+};
+
+class ShaderManager {
+public:
+    std::shared_ptr<Shader> GetShader(std::string path, ShaderType type) {
+
+        std::string shaderCode;
+        std::ifstream shaderStream(path, std::ios::in);
+        if (shaderStream.is_open()) {
+            std::string line = "";
+            while (getline(shaderStream, line)) {
+                shaderCode += "\n" + line;
+            }
+            shaderStream.close();
+        } else {
+            cout << "Не смогли открыть файл с шейдером : " << path.c_str() << endl;
+        }
+
+
+        GLuint shaderId;
+        if (type == ShaderType::Fragment) {
+            shaderId = glCreateShader(GL_FRAGMENT_SHADER);
+        } else {
+            shaderId = glCreateShader(GL_VERTEX_SHADER);
+        }
+
+
+        GLint Result = GL_FALSE;
+        int InfoLogLength;
+
+        cout << "Компиляция шейдера : " << path.c_str() << endl;
+        char const *shaderSourcePointer = shaderCode.c_str();
+        glShaderSource(shaderId, 1, &shaderSourcePointer, NULL);
+        glCompileShader(shaderId);
+
+        glGetShaderiv(shaderId, GL_COMPILE_STATUS, &Result);
+        glGetShaderiv(shaderId, GL_INFO_LOG_LENGTH, &InfoLogLength);
+        std::vector<char> shaderErrorMessage(InfoLogLength);
+        glGetShaderInfoLog(shaderId, InfoLogLength, NULL, &shaderErrorMessage[0]);
+        if (Result == GL_FALSE) {
+            cout << "Ошибка при компиляция шейдера : " << &shaderErrorMessage[0] << endl;
+        }
+
+        return std::make_shared<Shader>(shaderId);
+
+    }
+};
+
+
+class ShaderProgram {
+
+public:
+    ShaderProgram() {
+        _programID = glCreateProgram();
+    }
+    ShaderProgram &AttachShader(const std::shared_ptr<Shader> shader) {
+        glAttachShader(_programID, shader->get_shaderId());
+        return *this;
+    }
+    void Link() {
+        cout << "Линкуем  шейдерную программу" << endl;
+        glLinkProgram(_programID);
+        GLint Result = GL_FALSE;
+        int InfoLogLength;
+
+        glGetProgramiv(_programID, GL_LINK_STATUS, &Result);
+        glGetProgramiv(_programID, GL_INFO_LOG_LENGTH, &InfoLogLength);
+        std::vector<char> ProgramErrorMessage(std::max(InfoLogLength, int(1)));
+        glGetProgramInfoLog(_programID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
+        if (Result == GL_FALSE) {
+            cout << "Ошибка при линковании шейдерной программы : " << &ProgramErrorMessage[0] << endl;
+        }
+    }
+    void Use() {
+        glUseProgram(_programID);
+    }
+
+    ~ShaderProgram() {
+        glDeleteProgram(_programID);
+    }
+
+private:
+    GLuint _programID;
+};
+
+void PrintOpenGlVersion() {
+    const GLubyte *version = glGetString(GL_VERSION);
+    const GLubyte *vendor = glGetString(GL_VENDOR);
+    const GLubyte *renderer = glGetString(GL_RENDERER);
+
+    const GLubyte *glslVersion = glGetString(GL_SHADING_LANGUAGE_VERSION);
+
+    cout << "Renderer: " << renderer << endl;
+    cout << "Vendor: " << vendor << endl;
+    cout << "OpenGL Version: " << version << endl;
+    cout << "glsl Version: " << glslVersion << endl;
+
+    GLint numb_extensions;
+
+    glGetIntegerv(GL_NUM_EXTENSIONS, &numb_extensions);
+    cout << "Extensions: " << numb_extensions << " \n" << endl;
+}
+
+
 // Это будет идентификатором нашего буфера вершин
 GLuint vertexbuffer;
 
 
-
-GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path){
-
-    GLuint ProgramID = glCreateProgram();
-
-    // Создаем шейдеры
-    GLuint VertexShaderID = glCreateShader(GL_VERTEX_SHADER);
-    GLuint FragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
-
-    // Загружаем код Вершинного Шейдера из файла
-    std::string VertexShaderCode;
-    std::ifstream VertexShaderStream(vertex_file_path, std::ios::in);
-    if(VertexShaderStream.is_open())
-    {
-        std::string Line = "";
-        while(getline(VertexShaderStream, Line))
-            VertexShaderCode += "\n" + Line;
-        VertexShaderStream.close();
-    }else
-    {
-        printf("Не смогли открыть вершинный шейдер : %s\n", vertex_file_path);
-    }
-
-    // Загружаем код Фрагментного шейдера из файла
-    std::string FragmentShaderCode;
-    std::ifstream FragmentShaderStream(fragment_file_path, std::ios::in);
-    if(FragmentShaderStream.is_open()){
-        std::string Line = "";
-        while(getline(FragmentShaderStream, Line))
-            FragmentShaderCode += "\n" + Line;
-        FragmentShaderStream.close();
-    }else
-    {
-        printf("Не смогли открыть Фрагментный шейдер : %s\n", vertex_file_path);
-
-    }
-
-    GLint Result = GL_FALSE;
-     int InfoLogLength;
-
-    // Компилируем Вершинный шейдер
-    printf("Компиляция шейдера: %s\n", vertex_file_path);
-    char const * VertexSourcePointer = VertexShaderCode.c_str();
-    glShaderSource(VertexShaderID, 1, &VertexSourcePointer , NULL);
-    glCompileShader(VertexShaderID);
-
-    // Выполняем проверку Вершинного шейдера
-    glGetShaderiv(VertexShaderID, GL_COMPILE_STATUS, &Result);
-    glGetShaderiv(VertexShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    std::vector<char> VertexShaderErrorMessage(InfoLogLength);
-    glGetShaderInfoLog(VertexShaderID, InfoLogLength, NULL, &VertexShaderErrorMessage[0]);
-    if(Result == GL_FALSE) {
-        fprintf(stdout, "%s\n", &VertexShaderErrorMessage[0]);
-    }
-    // Компилируем Фрагментный шейдер
-    printf("Компиляция шейдера: %s\n", fragment_file_path);
-    char const * FragmentSourcePointer = FragmentShaderCode.c_str();
-    glShaderSource(FragmentShaderID, 1, &FragmentSourcePointer , NULL);
-    glCompileShader(FragmentShaderID);
-
-    // Проверяем Фрагментный шейдер
-    glGetShaderiv(FragmentShaderID, GL_COMPILE_STATUS, &Result);
-    glGetShaderiv(FragmentShaderID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    std::vector<char> FragmentShaderErrorMessage(InfoLogLength);
-    glGetShaderInfoLog(FragmentShaderID, InfoLogLength, NULL, &FragmentShaderErrorMessage[0]);
-    if(Result == GL_FALSE) {
-        fprintf(stdout, "%s\n", &FragmentShaderErrorMessage[0]);
-    }
-    // Создаем шейдерную программу и привязываем шейдеры к ней
-    fprintf(stdout, "Создаем шейдерную программу и привязываем шейдеры к ней\n");
-
-    glAttachShader(ProgramID, VertexShaderID);
-    glAttachShader(ProgramID, FragmentShaderID);
-    glLinkProgram(ProgramID);
-
-    // Проверяем шейдерную программу
-    glGetProgramiv(ProgramID, GL_LINK_STATUS, &Result);
-    glGetProgramiv(ProgramID, GL_INFO_LOG_LENGTH, &InfoLogLength);
-    std::vector<char> ProgramErrorMessage( std::max(InfoLogLength, int(1)) );
-    glGetProgramInfoLog(ProgramID, InfoLogLength, NULL, &ProgramErrorMessage[0]);
-    if(Result == GL_FALSE) {
-        fprintf(stdout, "%s\n", &ProgramErrorMessage[0]);
-    }
-    glDeleteShader(VertexShaderID);
-    glDeleteShader(FragmentShaderID);
-
-    return ProgramID;
-}
-
-void  Draw()
-{
+void Draw() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 // Указываем, что первым буфером атрибутов будут вершины
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+
     glVertexAttribPointer(
             0,                  // Атрибут 0. Подробнее об этом будет рассказано в части, посвященной шейдерам.
             3,                  // Размер
             GL_FLOAT,           // Тип
             GL_FALSE,           // Указывает, что значения не нормализованы
             0,                  // Шаг
-            (void*)0            // Смещение массива в буфере
+            (void *) 0            // Смещение массива в буфере
     );
 
 // Вывести треугольник!
-    glDrawArrays(GL_TRIANGLES, 0, 3); // Начиная с вершины 0, всего 3 вершины -> один треугольник
+   glDrawArrays(GL_TRIANGLES, 0, 4); // Начиная с вершины 0, всего 3 вершины -> один треугольник
 
     glDisableVertexAttribArray(0);
-}
-
-
-void InitGlDraw() {
-    GLuint VertexArrayID;
-    glGenVertexArrays(1, &VertexArrayID);
-    glBindVertexArray(VertexArrayID);
-}
-void Init(int width, int height)
-{
-    glViewport(0, 0, (GLsizei) width, (GLsizei) height);
-    glEnable(GL_DEPTH_TEST);
-    glDepthMask(GL_TRUE);
-    glDepthFunc(GL_LEQUAL);
-    glDepthRange(0.0f, 10.0f);
-
-    glEnable (GL_BLEND);
-    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 #ifdef _WIN32
@@ -152,60 +214,39 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 int main(int argc, char* argv[])
 #endif
 {
-  //  auto _log = std::make_shared<Logger>();
-
-    SDL_Window * gWindow = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
-    if( gWindow == nullptr ) {
+    SDL_Window *gWindow = SDL_CreateWindow("Game", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL);
+    if (gWindow == nullptr) {
         //_log-> Error("Window could not be created! SDL Error: "+ std::string(SDL_GetError()));
 
     }
 
-    SDL_GLContext  glContext  =  SDL_GL_CreateContext(gWindow);
-    if( glContext == nullptr ) {
-      //  _log-> Error("GLContext could not be created! SDL Error: "+ std::string(SDL_GetError()));
+    SDL_GLContext glContext = SDL_GL_CreateContext(gWindow);
+    if (glContext == nullptr) {
+        //  _log-> Error("GLContext could not be created! SDL Error: "+ std::string(SDL_GetError()));
 
     }
 
-
     SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
-    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 24);
 
-    SDL_GL_SetAttribute(SDL_GL_MULTISAMPLEBUFFERS,1);
+    PrintOpenGlVersion();
 
-    const GLubyte *version=glGetString(GL_VERSION);
-    const GLubyte *vendor= glGetString(GL_VENDOR);
-    const GLubyte *renderer=glGetString(GL_RENDERER);
-
-    const GLubyte  *glslVersion =glGetString(GL_SHADING_LANGUAGE_VERSION);
-
-    cout<<"Renderer: " << renderer<<endl;
-    cout<<"Vendor: " << vendor<<endl;
-    cout<<"OpenGL Version: " << version<<endl;
-    cout<<"glsl Version: " << glslVersion<<endl;
-
-
-    GLint nombre_extensions;
-
-    glGetIntegerv(GL_NUM_EXTENSIONS, &nombre_extensions);
-    cout<<"Extensions: " <<nombre_extensions<<" \n"<<endl;
-
-// Initialize GLEW
+    // Initialize GLEW
     glewExperimental = true; // Needed for core profile
     if (glewInit() != GLEW_OK) {
         fprintf(stderr, "Failed to initialize GLEW\n");
 
     }
-    Init(SCREEN_WIDTH, SCREEN_HEIGHT);
-    InitGlDraw();
+
+    GLuint VertexArrayID;
+    glGenVertexArrays(1, &VertexArrayID);
+    glBindVertexArray(VertexArrayID);
 
 // Массив 3 векторов, которые являются вершинами треугольника
     static const GLfloat g_vertex_buffer_data[] = {
             -1.0f, -1.0f, 0.0f,
             1.0f, -1.0f, 0.0f,
-            0.0f,  1.0f, 0.0f,
+            0.0f, 1.0f, 0.0f,
     };
-
-
 
 // Создадим 1 буфер и поместим в переменную vertexbuffer его идентификатор
     glGenBuffers(1, &vertexbuffer);
@@ -216,12 +257,18 @@ int main(int argc, char* argv[])
 // Передадим информацию о вершинах в OpenGL
     glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data), g_vertex_buffer_data, GL_STATIC_DRAW);
 
+    ShaderManager shaderManager;
 
-// Создать и откомпилировать нашу шейдерную программу
-    GLuint programID = LoadShaders( "../code/test/OpenGl/rectangle_shaders/Simple.vertex.glsl", "../code/test/OpenGl/rectangle_shaders/Simple.fragment.glsl" );
-// Устанавливаем наш шейдер текущим
-    glUseProgram(programID);
-
+    auto vertexShader = shaderManager.GetShader("../code/test/OpenGl/rectangle_shaders/Simple.vertex.glsl", ShaderType::Vertex);
+    OpenGLCheckErrorMacro();
+    auto fragmentShader = shaderManager.GetShader("../code/test/OpenGl/rectangle_shaders/Simple.fragment.glsl", ShaderType::Fragment);
+    OpenGLCheckErrorMacro();
+    ShaderProgram shaderProgram;
+    OpenGLCheckErrorMacro();
+    shaderProgram.AttachShader(vertexShader).AttachShader(fragmentShader).Link();
+    OpenGLCheckErrorMacro();
+    shaderProgram.Use();
+    OpenGLCheckErrorMacro();
     bool _run = true;
     while (_run) {
 
@@ -232,7 +279,7 @@ int main(int argc, char* argv[])
 
         switch (event.type) {
             case SDL_QUIT : {
-                _run= false;
+                _run = false;
                 break;
             }
         }
