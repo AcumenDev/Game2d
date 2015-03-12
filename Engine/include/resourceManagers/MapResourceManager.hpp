a84a2ed4d30d393d4187aad32fe623b4d87b4257
Dmitry Leonov <dimonstar67rus@gmail.com>
2015-03-12 23:42:42 +0300

    #1 небольшие правки к предыдущему

--- Engine/include/resourceManagers/MapResourceManager.hpp
+++ Engine/include/resourceManagers/MapResourceManager.hpp
@@ -1,25 +1,29 @@
 #ifndef MAPRESOURCEMANAGER_H
 #define MAPRESOURCEMANAGER_H
 
-#include <c++/bits/shared_ptr.h>
+#include <memory>
+#include <fstream>
+#include "utils/logger/Logger.hpp"
+#include "Utils/fileSystem/FileSystemUtils.hpp"
 #include "gameObjects/GameObjectsFactory.hpp"
 #include "SceneManager.hpp"
 #include "BaseResourceManager.hpp"
 
-using std::shared_ptr
-using std::string
-using Logger
+using std::shared_ptr;
+using std::string;
 
-class MapResourceManager : public BaseResourceManager {
+class MapResourceManager : public BaseResourceManager<shared_ptr<SceneNode>> {
 public:
     MapResourceManager(
             shared_ptr<SceneManager> sceneManager,
             shared_ptr<GameObjectsFactory> gameObjFactory,
             shared_ptr<SystemSettings> systemSettings
     );
+
     virtual ~MapResourceManager();
-    //TODO Реализация будет возвращать scenenode с расставленными gameobject-ами
+
     shared_ptr<SceneNode> getResourse(string name) override;
+
 private:
     string _name;
     shared_ptr<SceneManager> _sceneManager;
--- Engine/src/resourceManagers/MapResourceManager.cpp
+++ Engine/src/resourceManagers/MapResourceManager.cpp
@@ -4,11 +4,11 @@ MapResourceManager::MapResourceManager(
         shared_ptr<SceneManager> sceneManager,
         shared_ptr<GameObjectsFactory> gameObjFactory,
         shared_ptr<SystemSettings> systemSettings) :
+        _name( "MapResourceManager"),
         _sceneManager(sceneManager),
         _gameObjFactory(gameObjFactory),
         _systemSettings(systemSettings) {
-    _name = "MapResourceManager";
-}
+    }
 
 MapResourceManager::~MapResourceManager() {
 }
@@ -16,14 +16,14 @@ MapResourceManager::~MapResourceManager() {
 shared_ptr<SceneNode> MapResourceManager::getResourse(string name) {
         auto path = FileSystem::Path::Combine(_systemSettings->get_resFolder(), name);
         Logger::Get()->Debug(_name,"Loading map from: "+ path);
-        std::ifstream spriteAnimation(path.c_str());
-        if (!spriteAnimation.is_open()) {
+        std::ifstream map(path.c_str());
+        if (!map.is_open()) {
                 Logger::Get()->Error(_name, "Couldn't load map file : " + path);
                 return nullptr;
         }
         auto sceneNode = _sceneManager->AddChildNode("MainNode");
 
-        //TODO тут будет код чтения данных карты из бинарного файла(???) и создание на их основе gameobject-ов + добавление их в sceneNode
+        //TODO С‚СѓС‚ Р±СѓРґРµС‚ РєРѕРґ С‡С‚РµРЅРёСЏ РґР°РЅРЅС‹С… РєР°СЂС‚С‹ РёР· Р±РёРЅР°СЂРЅРѕРіРѕ С„Р°Р№Р»Р°(???) Рё СЃРѕР·РґР°РЅРёРµ РЅР° РёС… РѕСЃРЅРѕРІРµ gameobject-РѕРІ + РґРѕР±Р°РІР»РµРЅРёРµ РёС… РІ sceneNode
 
         return sceneNode;
 }
